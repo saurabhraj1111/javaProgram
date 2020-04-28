4d050092c31e11fe89da0703160e9656552ea7c5
saurabh <saurabhraj1111@gmail.com>
2020-04-28 20:53:59 +0530

    First world capital program

---
+++ src/com/test/anagtram/FirstWordCapital.java
@@ -0,0 +1,33 @@
+package com.test.anagtram;
+
+public class FirstWordCapital {
+
+	public static void main(String[] args) {
+
+		String str = getFormatedStr("hello saurabh raj");
+		System.out.println(str);
+	}
+
+	private static String getFormatedStr(String str) {
+		// TODO Auto-generated method stub
+		String result = "";
+		String[] strArr = str.split("\\s");
+
+		for (int i = 0; i < strArr.length; i++) {
+			// System.out.println("" + strArr[i]);
+			String curStr = strArr[i];
+			String currResult = "";
+			for (int j = 0; j < curStr.length(); j++) {
+				if (j == 0) {
+					char first = curStr.charAt(0);
+					currResult += Character.toString(first).toUpperCase();
+				} else {
+					currResult += Character.toString(curStr.charAt(j));
+				}
+			}
+			result += currResult + " ";
+		}
+
+		return result.trim();
+	}
+}
